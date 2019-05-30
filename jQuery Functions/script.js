99e4663979c75a57d27a7192ee5d27c9da7c1158
Kent <zelink99@gmail.com>
2019-05-30 15:23:13 -0500

    First Upload
    First upload of six basic jQuery examples.

---
+++ jQuery Functions/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('.show p').hide();
+    $('.slideDown p').slideUp();
+    
+    $('.show button').click(function() { //show
+        $('.show p').show();
+    })
+    $('.addClass button').click(function() { //addClass red
+        $('.addClass p').addClass("red");
+    })
+    $('.hide button').click(function() { //hide
+        $('.hide p').hide();
+    })        
+    $('.toggle button').click(function() { //toggle
+        $('.toggle p').toggle();
+    })
+    $('.slideDown button').click(function() { //slideDown
+        $('.slideDown p').slideDown();
+    })
+    $('.slideUp button').click(function(){ //slideUp
+        $('.slideUp p').slideUp();
+    })
+})
