e29082254cdbc9ddb1b237eb37ce3cb9693f2d90
Kent <zelink99@gmail.com>
2019-05-30 16:41:20 -0500

    Working

---
+++ Disappearing Ninja/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('.ninja').click(function() { //hide
+        $(this).hide();
+    })
+    // $('.ninja2').click(function() { //hide
+    //     $(this).hide();
+    // })
+    // $('.ninja3').click(function() { //hide
+    //     $(this).hide();
+    // })
+    // $('.ninja4').click(function() { //hide
+    //     $(this).hide();
+    // })
+    // $('.ninja5').click(function() { //hide
+    //     $(this).hide();
+    // })
+    // $('.ninja6').click(function() { //hide
+    //     $(this).hide();
+    // })
+    // $('.ninja7').click(function() { //hide
+    //     $(this).hide();
+    // })
+    // $('.ninja8').click(function() { //hide
+    //     $(this).hide();
+    // })
+    $('#RESET').click(function() {
+        $('.ninja').show();
+        // $('.ninja2').show();
+        // $('.ninja3').show();
+        // $('.ninja4').show();
+        // $('.ninja5').show();
+        // $('.ninja6').show();
+        // $('.ninja7').show();
+        // $('.ninja8').show();
+    })
+})
