6805fcea834ca8e10745b8442b9d621988f02881
Kent <zelink99@gmail.com>
2019-05-31 14:56:40 -0500

    Test

---
+++ Python Stack/my_environments/py3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\zelin\desktop\code\upload~1\kent_j~1\python~1\my_env~1\py3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
