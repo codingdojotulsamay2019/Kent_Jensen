755733c5df7c0c9c918e3ffbb7dd5f9fbd4c49f2
Kent <zelink99@gmail.com>
2019-06-03 17:51:42 -0500

    update

--- Python Stack/python/fundamentals/userClass_chained.py
+++ Python Stack/python/fundamentals/userClass_chained.py
@@ -5,17 +5,17 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):
     	self.account_balance += amount
-        return self	                    
+        return self.self
     def make_withdrawal(self,amount):
         self.account_balance -= amount
-        return self
+        return self.self
     def display_user_balance(self): 
         print("User: ", self.name, ", Balance: $", self.account_balance)
-        return self 
+        return self.self
     def transfer_money(self, other_user, amount):   
         self.account_balance -= amount
         other_user.account_balance += amount     
-        print("Transferring $", amount)                                       
+        print("Transferring $", amount)
 
 guido = User("Guido van Rossum","guido@python.com")
 monty = User("Monty Python", "monty@python.com")
