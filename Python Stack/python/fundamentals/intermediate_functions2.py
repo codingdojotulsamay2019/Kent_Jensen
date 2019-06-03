f6648837dfab9e509812f577fcfe7350d364c4c8
Kent <zelink99@gmail.com>
2019-06-02 23:44:36 -0500

    first try

---
+++ Python Stack/python/fundamentals/intermediate_functions2.py
@@ -0,0 +1,22 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+def tenToFifteen(x):
+    x.pop(x[1][0])
+    x.insert((x[1][0]),15)
+    print(x)
+    return x
+
+tenToFifteen()
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+# In the sports_directory, change 'Messi' to 'Andres'
+# Change the value 20 in z to 30
