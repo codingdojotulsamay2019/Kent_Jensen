72f02faab3f898059c0de6ab533a0a0919e00046
Kent <zelink99@gmail.com>
2019-06-03 13:46:45 -0500

    working version

--- Python Stack/python/fundamentals/intermediate_functions2.py
+++ Python Stack/python/fundamentals/intermediate_functions2.py
@@ -1,3 +1,5 @@
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+
 x = [ [5,2,3], [10,8,9] ] 
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -9,14 +11,27 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
-def tenToFifteen(x):
-    x.pop(x[1][0])
-    x.insert((x[1][0]),15)
+def tenToFifteen():
+    x[1][0] = 15
     print(x)
     return x
-
 tenToFifteen()
-# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+
 # Change the last_name of the first student from 'Jordan' to 'Bryant'
+def change_last():
+    students[0].update({"last_name": "Bryant"})
+    print(students)
+    return(students)
+change_last()
 # In the sports_directory, change 'Messi' to 'Andres'
-# Change the value 20 in z to 30
+def sports_change():
+    sports_directory['soccer'][0]='Andres'
+    print(sports_directory)
+
+sports_change()
+
+# Change the value 20 in z to 30 ||   z = [ {'x': 10, 'y': 20} ]
+def changeZ():
+    z[0]['y'] = 30
+    print(z)
+changeZ()
