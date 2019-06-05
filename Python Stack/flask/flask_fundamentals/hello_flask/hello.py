52da8ee1c0108f47a272aeebf3adbb5d43e6f4cf
Kent <zelink99@gmail.com>
2019-06-05 13:24:02 -0500

    update

--- Python Stack/flask/flask_fundamentals/hello_flask/hello.py
+++ Python Stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -1,10 +1,10 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
+if __name__=="__main__":
+    app.run(debug=True)
 
 # import statements, maybe some other routes
     @app.route('/success')
