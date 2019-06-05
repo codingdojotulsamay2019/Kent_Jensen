c4c3cf7cb73874becb6c1b7183b32b2969ae8815
Kent <zelink99@gmail.com>
2019-06-05 17:01:55 -0500

    working

---
+++ Python Stack/flask/flask_fundamentals/Checkerboard/checkerboard.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, redirect, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", horizontal=int(8), vertical=int(8) )
+@app.route ('/<x>/<y>')
+def size(x,y):
+    return render_template("index.html", horizontal=int(x), vertical=int(y), color1="red", color2="black")
+@app.route ('/<x>/<y>/<input1>/<input2>')
+def size_color(x,y,input1,input2):
+    return render_template("index.html", horizontal=int(x), vertical=int(y), color1=input1, color2=input2)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+#EXPECTED:
+#localhost:5000/ should load an 8x8 checkerboard
+#localhost:5000/(x)/(y) should load an x by y checkerboard
