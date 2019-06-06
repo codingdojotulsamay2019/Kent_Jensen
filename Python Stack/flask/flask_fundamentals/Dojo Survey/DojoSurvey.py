f6d9341889187e59fe6fcbb7eb7adc4f0ea4059a
Kent <zelink99@gmail.com>
2019-06-06 15:23:39 -0500

    working version

--- Python Stack/flask/flask_fundamentals/Dojo Survey/DojoSurvey.py
+++ Python Stack/flask/flask_fundamentals/Dojo Survey/DojoSurvey.py
@@ -10,8 +10,11 @@ def result():
     print("Got POST info")
     print(request.form)
     print(f"Name submitted: {request.form['name']}")
-    print(f"Email submitted: {request.form['email']}")
-    return render_template("result.html", username=request.form["name"], em=request.form["email"])
+    print(f"Location submitted: {request.form['location']}")
+    print(f"Language submitted: {request.form['language']}")
+    print(f"Answer submitted: {request.form['learning']}")
+    print(f"Comment submitted: {request.form['comment']}")
+    return render_template("result.html", username=request.form["name"], loc=request.form["location"],lan=request.form["language"],learn=request.form["learning"],com=request.form['comment'])
 
 if __name__=="__main__":
     app.run(debug=True)
