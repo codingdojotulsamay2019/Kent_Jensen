f723e347ee16ae59546096b2677c6f1e5eb170c5
Kent <zelink99@gmail.com>
2019-06-11 13:27:24 -0500

    update

--- Python Stack/flask/flask_fundamentals/counter/server.py
+++ Python Stack/flask/flask_fundamentals/counter/server.py
@@ -1,33 +1,41 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)  
 app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
-counter=0
-@app.route('/')         
+@app.route('/')
 def index():
+    print("*"*80)
     print("counter increased from visit")
-    counter +=1
-    session['count'] = request.form['counter']
+    print("*"*80)
+    if "counter" in session:
+        session['counter'] +=1
+    else:
+        session['counter'] = 0
     return render_template("index.html")
 
-app.route('/reset')
+@app.route('/reset', methods=['POST'])
 def reset():
     print("*"*80)
     print("RESETTING COUNTER")
     print("*"*80)
-    session['count'] = 0
-    return redirect["index.html"]
-# @app.route('/users', methods=['POST'])
-# def ():
-#     print("Got Post Info")
-#     # Here we add two properties to session to store the name and email
-#     return redirect('/show')
-@app.route('/show')
+    session['counter'] = 0
+    return redirect("/")
+
+@app.route('/plustwo', methods=['POST'])
+def plustwo():
+    print("*"*80)
+    print("ADDING TWO")
+    print("*"*80)
+    session['counter'] +=1
+    return redirect("/")
+
+@app.route('/show', methods=['POST'])
 def show_user():
     return render_template('show.html')
-@app.route('/destroy_session')
+
+@app.route('/destroy_session', methods=['POST'])
 def destroy_session():
-    counter = 0
-    #delete the cookie?
+    session.clear()     # clears all keys
+    return redirect("/")
 
 if __name__=="__main__":   
     app.run(debug=True)    
---
+++ Python Stack/flask/flask_mysql/first_flask_mysql/first_flask.sql
@@ -0,0 +1,22 @@
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Andrew', 'C', 'Ship Captain', NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Gin', 'T', 'Professional Question Asker', NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Tim', 'P', 'Motivational Speaker', NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Jasmine', 'C', 'TDD Pro', NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Josh', 'H', 'Drone Pilot - All Ratings', NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Mason', 'C', 'Professional Not-Friend Friend', NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ('Cody', 'L', 'All-American Dad', NOW(), NOW());
+
+SELECT * FROM friends;
