83332fd71ecb2813004fefb68ac327e31ffe063b
Kent <zelink99@gmail.com>
2019-08-13 17:43:11 -0500

    update

--- MEAN/MongoDB/MessageBoard/server.js
+++ MEAN/MongoDB/MessageBoard/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+const flash = require('express-flash');
 const session = require('express-session');
-
+app.use(flash());
 app.use(session({
     secret: 'keyboardkitteh',
     resave: false,
@@ -21,6 +22,7 @@ const MessageSchema = new mongoose.Schema({
     content: {type: String, required: [true, "Message required"], minlength: [5, "Message must be at least 5 characters."]},
     comments: [CommentSchema]}, {timestamps: true}
     )
+const Comment = mongoose.model('Comment', CommentSchema);
 const Message = mongoose.model('Message', MessageSchema);
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/static"));
@@ -29,9 +31,14 @@ app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {  
     Message.find()
         .then(data => {
+            console.log(data)
             res.render("index", {message: data})})
-        .catch(err => res.json(err));
-});
+        .catch(err => {
+            for (var key in err.errors) {
+            req.flash('registration', err.errors[key].message);
+        }
+    })
+})
 app.post('/', (req, res) => {
     console.log("POSTING MESSAGE")
     const message = new Message();
@@ -39,9 +46,14 @@ app.post('/', (req, res) => {
     message.content = req.body.content;
     message.save()
         .then(newMessageData => console.log('message created: ', newMessageData))
-        .catch(err => console.log(err));
-    res.redirect('/');
+        .catch(err => {
+            for (var key in err.errors) {
+                req.flash('registration', err.errors[key].message);
+            }
+            res.redirect('/');
+        })
 })
+
 // THIS DOES THE SAME THING AS THE ONE ABOVE!
 // app.post('/', (req, res) => {
 //     console.log("POSTING MESSAGE")
@@ -50,17 +62,23 @@ app.post('/', (req, res) => {
 //         .catch(err => console.log(err));
 //     res.redirect('/');
 // })
-app.get('/comment', (req,res) => {
-    console.log('loading comments')
-    res.redirect('/')
-})
 
-app.post('/comment', (req,res) => {
-    console.log("POSTING COMMENT")
+app.post('/comment/:id', (req,res) => {
+    console.log("POSTING COMMENT");
+    console.log(req.params.id);
     Comment.create(req.body)
-        .then(newCommentData => console.log('comment created: ', newCommentData))
-        .catch(err => console.log(err));
-    res.redirect('/')
+        .then(newCommentData => {
+            console.log('comment created: ', newCommentData);
+            return Message.updateOne({_id: req.params.id}, {
+                $push: {comments: newCommentData}
+            })
+        .catch(err => {
+            for (var key in err.errors) {
+                req.flash('registration', err.errors[key].message);
+            }
+        })
+        .finally(() => res.redirect('/'))
+    })
 })
 
 app.listen(8000, () => console.log("listening on port 8000"));
