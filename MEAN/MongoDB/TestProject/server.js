56411a468929c124648a7269ed9e83d3e569a953
Kent <zelink99@gmail.com>
2019-08-19 10:49:45 -0500

    update

---
+++ MEAN/MongoDB/TestProject/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app= express();
+
+
+app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(express.json());
+
+
+app.listen(8000, () => console.log ("listening on port 8000"));
