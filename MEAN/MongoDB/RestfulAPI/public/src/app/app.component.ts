4e2fafd66584dc320bbcc3d5e04c1682a667f4fb
Kent <zelink99@gmail.com>
2019-08-22 14:34:43 -0500

    update

--- MEAN/MongoDB/RestfulAPI/public/src/app/app.component.ts
+++ MEAN/MongoDB/RestfulAPI/public/src/app/app.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Restful Tasks API';
+  loggedIn: boolean;
+  tasks = [];
   constructor(private _httpService: HttpService){}
+  ngOnInit(){
+    this.getTasksFromService()
+  };
+  getTasksFromService(){
+    let tempObservable = this._httpService.getTasks();
+    tempObservable.subscribe(data => {
+      console.log("Got our tasks!", data)
+      this.tasks = data["tasks"];
+      console.log(this.tasks);
+      this.loggedIn = true;
+    } )
+  }
+  getTaskByIDFromService(){
+    let tempObservable = this._httpService.getTaskByID();
+    tempObservable.subscribe(data => {
+      console.log("Got the task!", data);
+      this.tasks = data["task"];
+    })
+  }
+  createTaskFromService(data){
+    let tempObservable = this._httpService.createTask(data);
+    tempObservable.subscribe(data => {
+      console.log("Creating task", data);
+    })
+  }
+  updateTaskFromService(data) {
+    let tempObservable = this._httpService.updateTask(data);
+    tempObservable.subscribe(data =>{
+    console.log("Updating task",data);
+  })
+  }
+  deleteTaskFromService(){
+    let tempObservable = this._httpService.deleteTask();
+    tempObservable.subscribe(data => {
+      console.log("Deleting task");
+    })
+  }
 }
+
 
--- MEAN/MongoDB/RestfulAPI/public/src/app/http.service.ts
+++ MEAN/MongoDB/RestfulAPI/public/src/app/http.service.ts
@@ -7,28 +7,20 @@ import { HttpClient } from '@angular/common/http';
 
 export class HttpService {
   constructor(private _http: HttpClient){
-     this.getTasks();
    }
    getTasks(){
-   // our http response is an Observable, store it in a variable
-      let tempObservable = this._http.get('/tasks');
-   // subscribe to the Observable and provide the code we would like to do with our data from the response
-      tempObservable.subscribe(data => console.log("Got our tasks!", data));
+      return this._http.get('/tasks');
    }
    getTaskByID(){
-      let tempObservable = this._http.get('/task/:id');
-      tempObservable.subscribe(data => console.log("Got the task!", data));
+      return this._http.get('/task/:id');
    }
    createTask(data){
-      let tempObservable = this._http.post('/task/new', data);
-      tempObservable.subscribe(data => console.log("Adding a new task!", data));
+      return this._http.post('/task/new', data);
    }
    updateTask(data){
-      let tempObservable = this._http.put('/task/:id', data);
-      tempObservable.subscribe(data => console.log("Updated the task!", data));
+      return this._http.put('/task/:id', data);
    }
    deleteTask(){
-      let tempObservable = this._http.delete('/task/:id');
-      tempObservable.subscribe(data => console.log("Deleted the task!", data));
+      return this._http.delete('/task/:id');
    }
 }
